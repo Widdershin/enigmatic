134564e32b557647a0f6e02b5279a3b33d5a658e
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-13 10:17:35 +1200

    Add a few more specs for distance

--- test/distance.js
+++ test/distance.js
@@ -12,4 +12,22 @@ describe('distance', () => {
 
     assert.equal(calculatedDistance, 15);
   });
+
+  it('returns the distance between two points diagonally', () => {
+    const calculatedDistance = distance(
+      {x: 0, y: 0},
+      {x: 15, y: 15}
+    );
+
+    assert.equal(calculatedDistance.toFixed(2), 21.21);
+  });
+
+  it('returns 0 if the points are the same', () => {
+    const calculatedDistance = distance(
+      {x: 0, y: 0},
+      {x: 0, y: 0}
+    );
+
+    assert.equal(calculatedDistance, 0);
+  });
 });
